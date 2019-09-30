49994d3526bd47e4793dfe0da5dd3193d1756fe2
Shreyas Macwan <Smacwan1933conestogac.on.ca>
2019-09-30 01:28:25 -0500

    remove comment

---
+++ Assignment-1SQA/MyTriangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1SQA
+{
+    class MyTriangle
+    {
+        string choiceString;
+        int choice, triangleSideA, triangleSideB, triangleSideC;
+
+        public void getTriangleSides()
+        {
+
+            Console.WriteLine("1 for Enter Trianle 2 for Exit ");
+            choiceString = Console.ReadLine();
+            Int32.TryParse(choiceString, out choice);
+
+           
+
+
+
+
+
+
+        }
+    
+    }
+}
---
+++ NUnitTestAssignment1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnitTestAssignment1.Framework;
+
+namespace NUnitTestAssignment1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
